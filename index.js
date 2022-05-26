93fda9de4f638fe4eb0916d5d0615e173e171bd4
Tara Kumar Sunuwar <bipurna@gmail.com>
2022-05-26 21:45:05 +0200

    finished project

---
+++ index.js
@@ -0,0 +1,61 @@
+import datas from "./data.json" assert { type: "json" };
+
+const labels = datas.map((data) => data.day);
+const dataN = datas.map((data) => data.amount);
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      backgroundColor: [
+        "hsl(10, 79%, 65%)",
+        "hsl(10, 79%, 65%)",
+        "hsl(186, 34%, 60%)",
+        "hsl(10, 79%, 65%)",
+        "hsl(10, 79%, 65%)",
+        "hsl(10, 79%, 65%)",
+        "hsl(10, 79%, 65%)",
+      ],
+      borderColor: "rgb(255,255,20)",
+      borderRadius: "6",
+      borderSkipped: false,
+      data: dataN,
+    },
+  ],
+};
+const config = {
+  type: "bar",
+  label:'',
+  data: data,
+
+  options: {
+      plugins:{
+          legend:{
+              display:false,
+          }
+      },
+    
+    scales: {
+       
+      x: {
+        grid: {
+          display: false,
+          drawBorder:false,
+        },
+      },
+      y: {
+        beginAtZero: true,
+        grid: {
+          drawOnChartArea: false,
+          drawBorder:false,
+        },
+        ticks: {
+          display: false,
+        },
+       
+      },
+    },
+  },
+};
+
+const chart = new Chart(document.getElementById("container"), config);
