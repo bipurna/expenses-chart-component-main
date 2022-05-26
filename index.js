7a6de9e4893858b6e22015c33b094b015b540a25
Tara Kumar Sunuwar <bipurna@gmail.com>
2022-05-26 22:25:50 +0200

    json removed

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-import datas from "./data.json" assert { type: "json" };
-
+const datas =[
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ]
 const labels = datas.map((data) => data.day);
 const dataN = datas.map((data) => data.amount);
 
